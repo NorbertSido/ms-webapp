7aab0acd46438a72f1a39abcd89e3e6340c6a694
Sidó Norbert <sido.norbert93@gmail.com>
2024-03-05 11:59:46 +0100

    implement cards

--- src/app/Display/product-list/product-list.component.ts
+++ src/app/Display/product-list/product-list.component.ts
@@ -8,7 +8,25 @@ import { ProductService } from '../../Service/product.service';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  data: Product[] = [];
+  data: Product[] = [
+    new Product(1, "Apple", 50),
+    new Product(2, "Banana", 30),
+    new Product(3, "Orange", 40),
+    new Product(4, "Mango", 60),
+    new Product(5, "Grapes", 20),
+    new  Product(6, "Watermelon" ,70),
+    new Product(7, "Pineapple", 80),
+    new Product(8, "Strawberry", 90),
+    new Product(9, "Blueberry", 10),
+    new Product(10, "Raspberry", 15),
+    new Product(11, "Kiwi", 12),
+    new Product(12, "Avocado", 18),
+    new Product(13, "Cherry", 25),
+    new Product(14, "Plum", 35),
+    new Product(15, "Multiple Fruits", 60),
+    new Product(16, "Apple", 80),
+    new Product(17, "Banana", 90)
+  ];
 
   constructor(private service: ProductService) {}
 }
