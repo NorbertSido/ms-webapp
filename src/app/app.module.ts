9a6c78c18b6ac85ebfc9893a0dad6f74e9c8d59d
Sidó Norbert <sido.norbert93@gmail.com>
2024-03-07 17:18:24 +0100

    modification of list import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductCardComponent } from './Display/product-card/product-card.component';
-import { ProductListComponent } from './Display/product-list/product-list.component';
+import { ProductListComponent } from './CRUD/product-list/product-list.component';
 
 import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { ProductAddComponent } from './CRUD/product-add/product-add.component';
